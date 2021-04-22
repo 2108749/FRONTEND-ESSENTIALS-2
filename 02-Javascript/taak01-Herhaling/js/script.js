ad86dc43cbefabd3b83ed51bfd49008dd87fd0ac
Diego <diegoblom01@gmail.com>
2021-04-22 13:20:25 +0200

    opdracht

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,18 @@
 let strNaam;
 let boolNaamKlopt;
+strNaam = prompt("Hoe heet jij?")
+boolNaamKlopt = confirm("Heet je echt " + strNaam);
+if(boolNaamKlopt == true){
+    alert("Welkom!");
+}
+else{
+    strNaam = prompt("Nog een keer.. Hoe heet jij?");
+    boolNaamKlopt = confirm("Heet je echt " + strNaam);
+}
+if(boolNaamKlopt == true){
+    alert("hoi")
+
+}
+else{
+    alert("alsnog welkom")
+}
