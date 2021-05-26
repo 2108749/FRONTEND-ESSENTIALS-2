b46a663199c2d58ef54df6ef6f55354489b92d7f
Diego <diegoblom01@gmail.com>
2021-05-26 13:49:25 +0200

    opdracht

--- 02-Javascript/taak13-Loops-Fibonacci/js/script.js
+++ 02-Javascript/taak13-Loops-Fibonacci/js/script.js
@@ -1,2 +1,19 @@
-var fibonacci = [0,1];
+const number = parseInt(prompt('Put here a number: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+console.log(n1);
+console.log(n2);
+
+nextTerm = n1 + n2;
+
+while (nextTerm <= number) {
+
+ 
+    console.log(nextTerm);
+
+    n1 = n2;
+    n2 = nextTerm;
+    nextTerm = n1 + n2;
+}
 
