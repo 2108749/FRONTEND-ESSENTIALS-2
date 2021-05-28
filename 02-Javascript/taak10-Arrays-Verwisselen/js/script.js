f245607f12adebe72f3b9972ae1a9eac39a442db
Diego <diegoblom01@gmail.com>
2021-05-28 10:55:00 +0200

    verbeteren

--- 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
+++ 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
@@ -17,26 +17,3 @@ document.querySelector(".film4-naam").innerHTML = topFilms[3];
 document.querySelector(".film5-ranking").innerHTML = 5;
 document.querySelector(".film5-naam").innerHTML = topFilms[4];
 
-window.onload = function () {
-	var upLink = document.querySelectorAll(".up");
-
-	for (var i = 0; i < upLink.length; i++) {
-		upLink[i].addEventListener('click', function () {
-			var wrapper = this.parentElement;
-
-			if (wrapper.previousElementSibling)
-			    wrapper.parentNode.insertBefore(wrapper, wrapper.previousElementSibling);
-		});
-	}
-
-	var downLink = document.querySelectorAll(".down");
-
-	for (var i = 0; i < downLink.length; i++) {
-		downLink[i].addEventListener('click', function () {
-			var wrapper = this.parentElement;
-
-			if (wrapper.nextElementSibling)
-			    wrapper.parentNode.insertBefore(wrapper.nextElementSibling, wrapper);
-		});
-	}
-}
